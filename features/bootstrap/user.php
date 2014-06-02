4fe17eb1eaa52389d96f7d114a8efcfa24dab5c9
Will McKenzie <will@komododigital.co.uk>
2014-06-02 15:41:37 +0100

    Set port in user class (need to make this load from behat.yml)

--- features/bootstrap/user.php
+++ features/bootstrap/user.php
@@ -6,11 +6,11 @@ class User {
 
     use Siesta;
 
-    const SIESTA_URL = "http://localhost:8000";
+    const SIESTA_URL = "http://localhost:9999";
     const SIESTA_ENDPOINT = "users";
 
     function __construct($data)
-    {    
+    {
         $this->_id = $data['id'];
         $this->name = $data['name'];
         $this->email = $data['email'];
