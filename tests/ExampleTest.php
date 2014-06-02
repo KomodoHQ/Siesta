30a7eb3df428e2c1f34a4f6b76d0cf7e6f718eb7
Will McKenzie <will@komododigital.co.uk>
2014-06-02 10:17:14 +0100

    Initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * Example test class, if you need to extend the Laravel default TestCase for
+ * extend the QuickjobAppTestCase, otherwise extend the 
+ * PHPUnit_Framework_TestCase
+ *
+ **/
+class ExampleTest extends PHPUnit_Framework_TestCase
+{
+	public function test_if_true()
+	{
+		$this->assertTrue(true);
+	}
+} // END class ExampleTest
